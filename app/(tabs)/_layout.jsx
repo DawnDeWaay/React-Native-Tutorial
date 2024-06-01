f6d09415254a8c5beaf4b9483649429477ddaf18
DonDeWaay <dondewaay@gmail.com>
2024-05-31 21:54:59 -0500

    Layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -2,19 +2,22 @@ import { View, Text, Image } from "react-native";
 import { Tabs, Redirect } from "expo-router";
 import React from "react";
 
-const TabIcon = ({icon, color, name, focused}) => {
-  return (
-    <View></View>
-  )
-}
+const TabIcon = ({ icon, color, name, focused }) => {
+  return <View></View>;
+};
 
 const TabsLayout = () => {
   return (
     <>
       <Tabs>
-        <Tabs.Screen name="home" options={{ title: "Home", headerShown: false, tabBarIcon:({color, focused}) => (
-<TabIcon />
-        ) }} />
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => <TabIcon />,
+          }}
+        />
       </Tabs>
     </>
   );
